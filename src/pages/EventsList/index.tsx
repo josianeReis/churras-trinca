c47884fbacf872dda8a67952f8a1923392b814b9
JosianeReis <josiane-reis@outlook.com>
2021-03-28 14:11:33 -0300

    Add events list page

--- src/pages/EventsList/index.tsx
+++ src/pages/EventsList/index.tsx
@@ -3,10 +3,26 @@ import CardItem from '../../components/CardItem';
 
 import './EventsList.module.scss';
 
+// const classesLabel = [
+//   {
+//     id: 1,
+//     name: 'josiane',
+//   },
+//   {
+//     id: 2,
+//     name: 'patricia',
+//   },
+//   {
+//     id: 3,
+//     name: 'marcio',
+//   },
+// ];
+
 const EventsList: React.FC = () => {
   return (
     <div id="page-event-list" className="container">
-      <div className="card-list-container">
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <CardItem />
         <CardItem />
         <CardItem />
         <CardItem />
